94b798a76cf89b46cf464f823b7fdff8e6a78356
kanat <esenalievkanat.90@gmail.com>
2025-02-28 00:29:12 +0600

    Переключение с Lesson44Server на Lesson45Server

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
-import lesson44.Lesson44Server;
+import lesson44.Lesson45Server;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson44Server("localhost", 9889).start();
+            new Lesson45Server("localhost", 9889).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
