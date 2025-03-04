8b91778903ce14295f98b09f4344439be0488414
kanat <esenalievkanat.90@gmail.com>
2025-03-04 08:35:18 +0600

    Обновлен класс Main для использования Lesson46Server

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
-import lesson44.Lesson45Server;
+import lesson44.Lesson46Server;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson45Server("localhost", 9889).start();
+            new Lesson46Server("localhost", 9889).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
